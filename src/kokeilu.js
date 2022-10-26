59fa145fa93ae25c1aca7b85e8f56cc1e456f518
TaneliSaa <82142073+TaneliSaa@users.noreply.github.com>
2022-10-27 02:24:06 +0300

    Muutoksia

--- src/kokeilu.js
+++ src/kokeilu.js
@@ -1,173 +1,127 @@
 import { useEffect, useState } from "react"
 
-const Asiakas = () => {
+const Kirjautuminen = () => {
 
-    const [nimi,setNimi] = useState("");
 
+    const [etunimi,setEtunimi] = useState("");
+    const [sukunimi,setSukunimi] = useState("");
     const [osoite,setOsoite] = useState("");
-
-    const [query,setQuery] = useState("");
-
-    const [asiakkaat,setAsiakkaat] = useState([]);
-
-    const [tyyppi,setTyyppi] = useState("0");
-
-    const [laskuri,setLaskuri] = useState(0);
-
-    useEffect( () => {
-
-        //console.log("useEffectiin menty, jossa haetaan asiakkaat");
-
-        const fetchAsiakkaat = async () => {
-
-            //console.log("Fetch asiakkaat alkaa...");
-
-            let response = await fetch("http://localhost:3004/asiakas/" + query);
-
-            //console.log("Fetch called...", response);
-
-            let c = await response.json();
-
-            console.log(c);
-
-            setAsiakkaat(c);
-            
-        }
-
-        if (laskuri > 0) {
-
-            fetchAsiakkaat();
-            
-        }
-
-    },[laskuri]);
-
-
-    const handleFetch = () => {
-
-        let m = "";
-
-        if (nimi == "" && osoite == "" && tyyppi == "0") {
-            m = "";
-            
-        }
-
-        else if (nimi != "" && osoite != "" && tyyppi =="") {
-            m = m = "?nimi=" + nimi + "&osoite=" + osoite + "&tyyppi_id=" + tyyppi;
-            
-        }
-
-        else if (nimi != "" && osoite != "") {
-            m = "?nimi=" + nimi + "&osoite=" + osoite;
-            
-        }
-        
-        else if (nimi != "") {
-            m = "?nimi=" + nimi;
-            
-        }
-
-        else if (osoite != "") {
-            m = "?osoite=" + osoite;
-            
-        }
-
-        else if (tyyppi != "") {
-            m = "?tyyppi_id=" + tyyppi;
-            
-        }
-
-        setQuery(m);
-
-    }
+    const [postinro,setPostinro] = useState("");
+    const [postitmp,setPostitmp] = useState("");
+    const [sahkoposti,setSahkoposti] = useState("");
+    const [kirjautumisForm,setKirjautumisForm] = useState(true);
+    const [rekisteroitymisForm,setRekisteroitymisForm] = useState(false);
     
-    const data = asiakkaat.map((item,index) => {
-
-        return (
-            <tr key={index}>
-                <td>{item.AVAIN}</td>
-                <td>{item.NIMI}</td>
-                <td>{item.OSOITE}</td>
-                <td>{item.POSTINRO}</td>
-                <td>{item.POSTITMP}</td>
-            </tr>
-        );
-    });
 
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    }
+
+
+
 
 
     return (
 
         <div>
 
-            <form onSubmit={(e) => handleSubmit(e)}>
+            {
+                kirjautumisForm ?
 
-                <label>
-                    Nimi:
-                    <input type="text" data-testid="nameInput" onChange={(e) => setNimi(e.target.value)}></input>
-                </label>
+                <form>
 
-                <label>
-                    Osoite:
-                    <input type="text" data-testid="addressInput" onChange={(e) => setOsoite(e.target.value)}></input>
-                </label>
+                    <label>
+                        Sähköposti
+                        <input type="text"></input>
+                    </label>
 
-                <button data-testid="searchButton" onClick={() => {setLaskuri(laskuri + 1) ; handleFetch()}}>Hae</button>
+                    <label>
+                        Salasana
+                        <input type="password"></input>
+                    </label>
+                    
+                    <button>Kirjaudu</button>
 
-                <label>
-                    Asiakastyypit:
-                    <select data-testid="customertypeSelect" onChange={(e) => setTyyppi(e.target.value)}>
-                        <option data-testid="customertypeOption" value="0">Valitse</option>
-                        <option data-testid="customertypeOption" value="1">1</option>
-                        <option data-testid="customertypeOption" value="2">2</option>
-                        <option data-testid="customertypeOption" value="3">3</option>
-                    </select>
-                </label>
+                    <button onClick={() => {setKirjautumisForm(false) ; setRekisteroitymisForm(true)}}>Rekisteröidy</button>
+                </form>
 
+                : null
+            }
+
+
+            {
+                rekisteroitymisForm ? 
+
+                <form>
+                    
+                    <label>
+                        Etunimi
+                        <input type="text"></input>
+                    </label>
+
+                    <br></br>
+
+                    <label>
+                        Sukunimi
+                        <input type="text"></input>
+                    </label>
+                    
+                    <br></br>
+
+                    <label>
+                        Osoite
+                        <input type="text"></input>
+                    </label>
+                    
+                    <br></br>
+
+                    <label>
+                        Postinro
+                        <input type="text"></input>
+                    </label>
+                    
+                    <br></br>
+
+                    <label>
+                        Postitoimipaikka
+                        <input type="text"></input>
+                    </label>
+                    
+                    <br></br>
+
+                    <label>
+                        Sähköposti
+                        <input type="text"></input>
+                    </label>
+                    
+                    <br></br>
+
+                    <label>
+                        Salasana
+                        <input type="text"></input>
+                    </label>
+                    
+                    <br></br>
+
+                    <button>Peruuta</button>
+                    <button>Rekisteröidy</button>
+            
+                </form>
+
+                : null
+            }    
                 
-
-            </form>
 
             
-            <table>
+            
 
-                <thead>
-
-                    {
-                        asiakkaat.length != 0 ?
-
-                        <tr>
-                            <th>Id</th>
-                            <th>Nimi</th>
-                            <th>Osoite</th>
-                            <th>Postinumero</th>
-                            <th>Postitoimipaikka</th>
-                        </tr>
-
-                        : null
-                    }
-                        
-                </thead>
-
-                <tbody>
-
-                    {data}
-
-                </tbody>
-
-            </table>
-
-                
+            
             
 
         </div>
-    )
+    );
 }
 
 export {
-    Asiakas
+    
+    Kirjautuminen
 }
+
